9be1390d8db962bc02ea1d91c43a040cf0b6e4e7
choejoonkyung <choejunkyung@gmail.com>
2021-04-18 17:30:03 +0900

    create project  struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { css } from '@emotion/react'
+import { css } from "@emotion/react";
 
 function App() {
-  return (
-    <div css={style}>
-      안뇽
-    </div>
-  );
+  return <div css={style}>안뇽</div>;
 }
 
 const style = css`
   background-color: black;
   font-size: 32px;
-`
+`;
 
 export default App;
