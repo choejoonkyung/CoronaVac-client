7844dae2c765292eb0410fa1f6bb5692981e467a
choejoonkyung <choejunkyung@gmail.com>
2021-04-18 17:19:32 +0900

    setting react, emotion

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react'
+
+function App() {
+  return (
+    <div css={style}>
+      안뇽
+    </div>
+  );
+}
+
+const style = css`
+  background-color: black;
+  font-size: 32px;
+`
+
+export default App;
