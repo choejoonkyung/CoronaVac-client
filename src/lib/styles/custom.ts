ef465a57433b89af948db4caf28437261ef3279d
choejoonkyung <choejunkyung@gmail.com>
2021-04-23 15:50:39 +0900

    add property to resetButton

--- src/lib/styles/custom.ts
+++ src/lib/styles/custom.ts
@@ -1,8 +1,9 @@
-import { css } from '@emotion/react'
+import { css } from "@emotion/react";
 
 export const resetButton = css`
   border: none;
   outline: none;
   background: none;
   padding: 0;
-`
+  cursor: pointer;
+`;
